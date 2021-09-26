adf8ec7efa584a033ab27806834d0c15b1debf5e
Bartlomiej Komendera <35965664+BartlomiejKomendera@users.noreply.github.com>
2021-09-26 16:53:48 +0200

    Main infrastructure + getting data from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IssuesComponent } from './issues/issues.component';
+import { IssueComponent } from './issues/issue/issue.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IssuesComponent,
+    IssueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
